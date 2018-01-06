b357e6df15436ed96e611d51ec35947020027ec4
siddharthphadke28 <sid.phadke@outlook.com>
2018-01-06 18:11:06 -0500

    Css changes

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = (env) => {
         }]
         },
         plugins: [
-            CSSExtract,
+            CSSExtract
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
